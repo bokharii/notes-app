9d69dc7f1392889c1eb06e88a2c9cc1398278bbf
Saaquib Bokhari <saaquibhbokhari@gmail.com>
2024-09-25 21:09:32 -0400

    added mongo conncetion

--- notes-app-backend/server.js
+++ notes-app-backend/server.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
@@ -39,7 +36,7 @@ app.post("/api/notes", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
